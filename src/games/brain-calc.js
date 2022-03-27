1afdf97d94083e1bf92c5a1788f3cd5a3dd3c387
medvedevvu <medvedevvu72@gmail.com>
2022-03-27 13:17:20 +0300

    п4.7 замечаний

--- src/games/brain-calc.js
+++ src/games/brain-calc.js
@@ -5,7 +5,7 @@ import {
 const getNewRandomCalc = () => {
   const a = Math.floor(Math.random() * 100);
   const b = Math.floor(Math.random() * 100);
-  const operationExecutionString = () => {
+  const getOperation = () => {
     const value = Math.floor(Math.random() * 10);
     let operationInStr = '';
     if (value >= 0 && value <= 3) {
@@ -17,23 +17,23 @@ const getNewRandomCalc = () => {
     }
     return operationInStr;
   };
-  const expressionStr = `${a} ${operationExecutionString()} ${b}`;
-  const execuExpressionFunc = () => {
-    const el = expressionStr.split(' ');
-    const aIn = Number.parseInt(el[0], 10);
-    const operation = el[1];
-    const bIn = Number.parseInt(el[2], 10);
-    let res = 0;
+  const expressionStr = `${a} ${getOperation()} ${b}`;
+  const getExecutionResult = () => {
+    const elementsOfExpression = expressionStr.split(' ');
+    const theFisrtParam = Number.parseInt(elementsOfExpression[0], 10);
+    const operation = elementsOfExpression[1];
+    const theSecondParam = Number.parseInt(elementsOfExpression[2], 10);
+    let result = 0;
     if (operation === '+') {
-      res = aIn + bIn;
+      result = theFisrtParam + theSecondParam;
     } else if (operation === '-') {
-      res = aIn - bIn;
+      result = theFisrtParam - theSecondParam;
     } else if (operation === '*') {
-      res = aIn * bIn;
+      result = theFisrtParam * theSecondParam;
     }
-    return res;
+    return result;
   };
-  return [expressionStr, execuExpressionFunc()];
+  return [expressionStr, getExecutionResult()];
 };
 
 const beginMessage = 'What is the result of the expression?';
